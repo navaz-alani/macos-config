c06b122bf367cf1817df5d043f7458b7ff9e042e
Navaz Alani <nalani@uwaterloo.ca>
2020-12-30 17:35:36 +0300

    cleaned up configuration

--- user_config.sh
+++ user_config.sh
@@ -1,19 +1,37 @@
 # user_config.sh contains any user specific settings that need to be loaded
+
 # flag to supress output
 quiet=
 
-# load custom scripts
-# custom functionality must be stored in this SC_DIR directory
 CONF_DIR="${HOME}/.config/$(whoami)_conf.d"
-SC_DIR="${CONF_DIR}/scripts"
-# todo: migrate programs from functions to scripts...
-PATH=$PATH:/Users/navaz/.config/navaz_conf.d/bin
+export CONF_DIR # child processes could need this...
+# scripts dir - bash functions (to be sourced)
+USER_SCRIPTS_DIR="${CONF_DIR}/scripts"
+# configs dir - configurations (to be sourced)
+USER_CONFIGS_DIR="${CONF_DIR}/configs"
+# user executables - shell programs, binaries (any executables needed in path)
+USER_BIN_DIR="${CONF_DIR}/bin"
+# append to path
+PATH="$PATH:${USER_BIN_DIR}"
 
-load_scripts() {
-  for sc in $(ls $SC_DIR/*.sh); do
-    source $sc;
-    [[ -z $quiet ]] && echo "> Loaded $sc";
+# single_source sources a single file - ${1} and echoes ${2} if `quiet` is not
+# set.
+single_source() {
+  if [[ -f ${1} ]]; then
+    source ${1};
+    [[ -z $quiet ]] && echo "> ${2}";
+  fi
+}
+
+# bulk_source sources the files in directory ${1} which have the extension ${2}
+# and echoes progress if `quiet` is not set.
+bulk_source() {
+  [[ -z ${quiet} ]] && echo "> Bulk loading ${1}/*${2}";
+  for f in $(ls ${1}/*${2}); do
+    source ${f};
+    [[ -z ${quiet} ]] && echo "> Loaded ${f}";
   done
+  [[ -z ${quiet} ]] && echo "> Bulk load complete\n";
 }
 
 # caffeine prevents the display from switching off
@@ -28,39 +46,16 @@ caffeine() {
   fi
 }
 
-# load_sensitive_aliases loads some private aliases, 
-# such as ssh shortcuts
-load_sensitive_aliases() {
-  sensitive=$CONF_DIR/sensitive_aliases.sh
-  if [[ -f $sensitive ]]; then
-    source $sensitive;
-    [[ -z $quiet ]] && echo "> Loaded sensitive aliases";
-  fi
-}
-
 [[ -z $quiet ]] && echo "Loading user configuration...";
-load_scripts;
-load_sensitive_aliases;
-caffeine;
-cleanup;
+
+# source user scripts
+bulk_source   ${USER_SCRIPTS_DIR} ".sh"
+# source user configs
+bulk_source   ${USER_CONFIGS_DIR} ".conf.sh"
+# source sensitive aliases
+single_source "${CONF_DIR}/sensitive_aliases.sh" "Loaded sensitive aliases."
+
+caffeine; # caffeinate system if not already done
+cleanup;  # script to remove unwanted files from $HOME
 clear;
 USER_CONF_LOADED="true";
-
-# conventient aliases
-## docker
-alias d="docker"
-alias dc="docker-compose"
-## brew
-alias bs="brew search"
-alias bi="brew install"
-alias bci="brew cask install"
-## default editor
-alias vi=nvim
-alias vim=nvim
-## wget
-# redirect config file to keep home dir clean
-alias wget="wget --hsts-file=~/.config/wget/.wget-hsts"
-
-# CONFIG
-EDITOR=nvim
-
