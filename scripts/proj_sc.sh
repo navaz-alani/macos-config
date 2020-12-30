c8085f84134695ad60ea8dfce4d9a8c5fc2aa3a0
Navaz Alani <nalani@uwaterloo.ca>
2020-12-30 16:32:42 +0300

    moved projects dir

--- scripts/proj_sc.sh
+++ scripts/proj_sc.sh
@@ -1,32 +1,27 @@
-#! /bin/bash
-
-# proj locates and navigates to a personal project directory
 proj() {
   if [[ -z $1 || $1 == "-h" || $1 == "-help" ]]; then
     echo "proj locates and navigates to a personal project directory";
     echo "usage: proj <project name>";
-    return 0;
   fi
 
-  # search go projects
+  # search golang projects
   go_proj_src=~/go/src/github.com/navaz-alani
   go_proj=$go_proj_src/$1
   if [[ -d $go_proj ]]; then
     cd $go_proj;
     echo "Switched to Golang Project '$1'";
-    return;
+    return 0;
   fi
 
-  # search github projects
-  git_proj_src=~/github.com/navaz-alani
-  git_proj=$git_proj_src/$1
-  if [[ -d $git_proj ]]; then
-    cd $git_proj;
-    echo "Switched to GitHub Project '$1'";
-    return;
+  # search projects directory
+  proj_src=~/projects
+  proj=$proj_src/$1
+  if [[ -d $proj ]]; then
+    cd $proj;
+    echo "Switched to Project '$1'";
+    return 0;
   fi
 
   echo "error: project not found";
   return 1;
 }
-
